1d7c46c34d84a2763c06f2032e3702162613966b
antonysantos9 <antony.g.santos9@aluno.senai.br>
2025-12-02 16:47:36 -0300

    Add files via upload

---
+++ exer01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+
+    int horas, minutos;
+
+    printf("digite as horas que voce quer converter: ");
+    scanf("%d", horas);
+
+    minutos = horas * 60;
+
+    printf("o resultado da conversao e igual a %d", minutos);
+
+
+     system("pause");
+    return 0;
+}
